c30f810ce4683b32576fa1fb2a0c29cc9f2177ea
subin <whtnqlsm1004@gmail.com>
2022-12-23 00:17:17 +0900

    api 연결

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,12 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { fakeCategory } from "../../api/youtubeApi";
 
 function Sidebar() {
+  const {
+    isLoading,
+    error,
+    data: categoryList,
+  } = useQuery(
+    ["categoryList"],
+    () => {
+      return fakeCategory();
+    },
+    { staleTime: 1000 * 60 * 1 },
+  );
+
   return (
     <aside className="flex-none w-64 border-r-1">
-      <ul>
-        <li>메뉴1</li>
-        <li>메뉴2</li>
-      </ul>
+      {categoryList && (
+        <ul className="text-lg p-2">
+          {categoryList.map((category) => (
+            <li key={category.id}>{category.snippet.title}</li>
+          ))}
+        </ul>
+      )}
     </aside>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,40 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+// import { useParams } from "react-router-dom";
+import { fakeSearch, searchYoutube } from "../api/youtubeApi";
+import Card from "../components/Card";
 
 function Home() {
-  return <div>홈</div>;
+  // const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: videoList,
+  } = useQuery(
+    ["videoList"],
+    () => {
+      console.log(fakeSearch());
+      return fakeSearch();
+    },
+    { staleTime: 1000 * 60 * 1 },
+  );
+
+  return (
+    <div className="flex-1 p-4 overflow-y-auto">
+      {isLoading && <p>로딩중....</p>}
+      {error && <p>문제가 발생했습니다.</p>}
+      {videoList && (
+        <>
+          <p className="text-xl font-semibold mb-2">인기 급상승</p>
+          <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 gap-y-5">
+            {videoList.map((video) => (
+              <Card key={video.id} video={video} />
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Home;
