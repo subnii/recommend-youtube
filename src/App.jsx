a7910837113b7c87246ae959261d18dc35adc724
subin <whtnqlsm1004@gmail.com>
2022-12-22 22:17:12 +0900

    리액트 쿼리 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Sidebar from "./components/layout/Sidebar";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
       <Header></Header>
-      <div className="flex">
-        <Sidebar></Sidebar>
-        <Outlet />
+      <div className="flex-1 flex h-full">
+        <QueryClientProvider client={queryClient}>
+          <Sidebar />
+          <Outlet />
+        </QueryClientProvider>
       </div>
     </>
   );
