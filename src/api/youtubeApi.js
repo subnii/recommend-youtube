6b16a40c71837c079c65298d38b387ff7ee5bd24
subin <whtnqlsm1004@gmail.com>
2022-12-30 00:30:08 +0900

    getCategory Api 추가

--- src/api/youtubeApi.js
+++ src/api/youtubeApi.js
@@ -2,25 +2,29 @@ import axios from "axios";
 import { Http } from "./core";
 
 export const searchYoutube = () => {
-  Http.get("/search?maxResults=25&q=방&part=snippet")
-    .then(function (response) {
+  return Http.get("/search?maxResults=25&part=snippet")
+    .then((response) => {
       return response.data.items.map((item) => ({ ...item, id: item.id.videoId }));
     })
-    .catch(function (error) {
-      // 에러 핸들링
+    .catch((error) => {
       console.log(error);
-    })
-    .then(function () {
-      // 항상 실행되는 영역
+    });
+};
+
+export const getCategory = () => {
+  return Http.get("/videoCategories?part=snippet&regionCode=KR&hl=ko_KR")
+    .then((response) => response.data.items)
+    .catch((error) => {
+      console.log(error);
     });
 };
 
 export const fakeSearch = () => {
-  return axios
-    .get("/videos/search.json")
-    .then((res) => res.data.items.map((item) => ({ ...item, id: item.id.videoId })));
+  return axios.get("/datas/search.json").then((res) => {
+    return res.data.items.map((item) => ({ ...item, id: item.id.videoId }));
+  });
 };
 
 export const fakeCategory = () => {
-  return axios.get("/videos/category.json").then((res) => res.data.items);
+  return axios.get("/datas/category.json").then((res) => res.data.items);
 };
