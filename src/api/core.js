6b1fa808d08920978dfc2f32f5071cfd4678858f
subin <whtnqlsm1004@gmail.com>
2023-01-04 01:45:06 +0900

    fake api추가

--- src/api/core.js
+++ src/api/core.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const Http = axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
-  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY, part: "snippet" },
 });
--- src/api/youtubeApi.js
+++ src/api/youtubeApi.js
@@ -1,8 +1,18 @@
 import axios from "axios";
 import { Http } from "./core";
 
+export const getHotVideo = () => {
+  return Http.get("/videos?part=statistics&chart=mostPopular&regionCode=KR")
+    .then((response) => {
+      return response.data.items.map((item) => ({ ...item, id: item.id.videoId }));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
 export const searchYoutube = () => {
-  return Http.get("/search?maxResults=25&part=snippet")
+  return Http.get("/search?maxResults=25")
     .then((response) => {
       return response.data.items.map((item) => ({ ...item, id: item.id.videoId }));
     })
@@ -12,11 +22,29 @@ export const searchYoutube = () => {
 };
 
 export const getCategory = () => {
-  return Http.get("/videoCategories?part=snippet&regionCode=KR&hl=ko_KR")
+  return Http.get("/videoCategories?regionCode=KR&hl=ko_KR")
     .then((response) => response.data.items)
     .catch((error) => {
       console.log(error);
     });
+};
+
+export const fakeHotVideo = () => {
+  return axios.get("/datas/hotvideo.json").then((res) => {
+    return res.data.items;
+  });
+};
+
+export const fakeChannel = () => {
+  return axios.get("/datas/channel.json").then((res) => {
+    return res.data.items[0];
+  });
+};
+
+export const fakeConnect = () => {
+  return axios.get("/datas/connect.json").then((res) => {
+    return res.data.items.map((item) => ({ ...item, id: item.id.videoId }));
+  });
 };
 
 export const fakeSearch = () => {
