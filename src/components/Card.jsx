f6da59aae4cc3c561d6f2b62a1abc834b3b790e4
subin <whtnqlsm1004@gmail.com>
2023-01-04 01:44:31 +0900

    타입 추가

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,33 @@ import React from "react";
 import * as timeago from "timeago.js";
 import TimeAgo from "timeago-react";
 import koLocale from "timeago.js/lib/lang/ko";
+import { getFixCount } from "../util/number";
+import { useNavigate } from "react-router-dom";
 
 timeago.register("ko", koLocale);
 
-function Card({ video }) {
+function Card({ video, type }) {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const navigate = useNavigate();
+
+  const clickHandler = () => {
+    navigate(`/detail/${video.id}`, { state: { video } });
+  };
+
   return (
-    <li>
-      <img src={thumbnails.medium.url} alt={title} className="w-full rounded-xl" />
+    <li className={type === "list" ? "flex gap-1 ml-2 mb-2" : ""} onClick={clickHandler}>
+      <img
+        className={type === "list" ? "w-60 mr-2 rounded-md" : "w-full rounded-xl"}
+        src={thumbnails.medium.url}
+        alt={title}
+      />
       <div>
-        <p className="font-semibold my-2">{title}</p>
+        <p className="font-semibold my-2 line-clamp-2">{title}</p>
         <p className="text-sm opacity-80">{channelTitle}</p>
+        {video.statistics && (
+          <span className="text-sm opacity-80">조회수 {getFixCount(video.statistics.viewCount)}회 &middot; </span>
+        )}
         <TimeAgo className="text-sm opacity-80" datetime={publishedAt} locale="ko" />
-        {/* <p className="text-sm opacity-80">{publishedAt}</p> */}
       </div>
     </li>
   );
