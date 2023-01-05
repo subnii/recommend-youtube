e613d0b8583954d397c33695e0be1fdfde7cae32
subin <whtnqlsm1004@gmail.com>
2023-01-05 16:44:04 +0900

    조건에 따라 api 분기처리

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,22 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
-import { fakeHotVideo, fakeSearch, getVideo, searchVideo } from "../api/youtubeApi";
+import { getVideo, searchVideo } from "../api/youtubeApi";
 import Card from "../components/Card";
 
 function Videos() {
-  const { keyword } = useParams();
+  const { keyword, id } = useParams();
   const {
     isLoading,
     error,
     data: videoList,
   } = useQuery(
-    ["videoList", keyword],
+    ["videoList", keyword, id],
     () => {
-      return keyword ? fakeSearch({ q: keyword }) : fakeHotVideo();
+      if (id) {
+        return getVideo({ videoCategoryId: id });
+      } else if (keyword) {
+        return searchVideo({ q: keyword });
+      } else {
+        return getVideo();
+      }
     },
-    { staleTime: 1000 * 60 * 1 },
+    { staleTime: 1000 * 60 * 5 },
   );
+
+  const getText = () => {
+    if (keyword) {
+      return "검색결과";
+    } else {
+      return "인기 급상승";
+    }
+  };
 
   return (
     <div className="flex-1 w-full h-full p-4 overflow-y-auto border-zinc-600 scrollbar-thin scrollbar-thumb-zinc-500 scrollbar-thumb-rounded scrollbar-track-zinc-900">
@@ -24,7 +38,7 @@ function Videos() {
       {error && <p>문제가 발생했습니다.</p>}
       {videoList && (
         <>
-          <p className="text-xl font-semibold mb-2">인기 급상승</p>
+          <p className="text-xl font-semibold mb-2">{getText()}</p>
           <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 gap-y-5">
             {videoList.map((video) => (
               <Card key={video.id} video={video} />
